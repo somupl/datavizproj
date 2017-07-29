90e0b76f83c6fcecb53ad39106586b0fc23e50f4
Somu Palaniappan <Somu Palaniappan>
2017-07-29 10:26:06 -0500

    all_4_chart_worked

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/barchart2');
+  response.render('pages/boxplot');
 });
 
 app.listen(app.get('port'), function() {
